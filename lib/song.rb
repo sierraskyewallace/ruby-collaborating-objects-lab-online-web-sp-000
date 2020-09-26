0999a515ecda16ada75ba01acd000c58adfc106a
Sierra Wallace <sskwallace@gmail.com>
2020-09-26 01:10:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,10 +3,9 @@ class Song
   @@all = [] 
   def initialize(name)
     @name = name
-    @@all << self 
   end 
   def self.all 
-    @@all
+    @@all << self 
   end
   def self.new_by_filename(file)
     song = self.new
