58b688da86c52f6251de48335acc511be085aaf9
Sierra Wallace <sskwallace@gmail.com>
2020-09-26 01:21:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ class Song
   end
   def self.new_by_filename(file)
     song = self.new
-    song.name = filen.chomp(".mp3").split(" - ")
+    song.name = file.chomp(".mp3").split(" - ")
     song
   end
   def artist_name=(name)
