22858e69f5c096fbe103fdef87cb0809f02365d0
Sierra Wallace <sskwallace@gmail.com>
2020-09-26 00:38:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,7 +13,7 @@ class Song
     song.name = filename.split(" - ")[1]
     song
 end
-  def artist_name=(name)
+  def artist=(name)
     self.artist_name = Artist.Find_or_create_by_name(name)
     artist.add_song(self)
   end
