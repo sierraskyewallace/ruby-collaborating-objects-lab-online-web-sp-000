b294faf218ba4969767ef40108f9b0254b16036e
Sierra Wallace <sskwallace@gmail.com>
2020-09-26 00:38:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
     song
 end
   def artist=(name)
-    self.artist_name = Artist.Find_or_create_by_name(name)
+    self.artist_name = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
 end
