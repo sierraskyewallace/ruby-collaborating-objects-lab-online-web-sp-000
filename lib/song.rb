570b6d41ae076c086105516b027175424ca2235b
Sierra Wallace <sskwallace@gmail.com>
2020-09-26 00:23:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,10 +9,5 @@ class Song
     @@all 
   end
   def self.new_by_filename(file)
-    artist = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
-    song.artist = artist
-    song
-  end
+    
 end
